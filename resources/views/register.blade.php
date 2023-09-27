9c5886bec74049b655a6f6b483bb90198c8e2fb9
ghifarit53 <mghifarit53@gmail.com>
2023-09-28 03:25:55 +0700

    first commit

---
+++ resources/views/register.blade.php
@@ -0,0 +1,6 @@
+@extends('layout.main')
+
+@section('container')
+    <h1 class="text-2xl font-bold">Register for a new account</h1>
+
+@endsection
---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,7 @@
+@extends('layout.main')
+
+@section('container')
+    <h1 class="text-2xl font-bold">Welcome to Tokopaedi</h1>
+
+    <p class="mt-2">Click <a class="text-green-600 underline" href="/register">here</a> to create an account if you don't already have one</p>
+@endsection
